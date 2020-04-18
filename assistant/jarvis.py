61350965eea7351d2462ee6df5374ebc007ae64c
akhileshsingh <trycatch55@gmail.com>
2020-04-18 20:31:09 +0530

    Application Feature Implemented

--- assistant/jarvis.py
+++ assistant/jarvis.py
@@ -2,6 +2,7 @@ import threading
 
 import speech_recognition as sr
 
+from intents.applications import Applications
 from intents.greeting import Greeting
 from utils.utils import Utils
 
@@ -14,6 +15,7 @@ class Jarvis:
         threading.Thread(target=self.run()).start()
 
     def read_voice_cmd(self):
+        print(...)
         voice_input = ''
         try:
             with sr.Microphone() as source:
@@ -32,16 +34,25 @@ class Jarvis:
         return voice_input.lower()
 
     def run(self):
+        session = False
         while True:
+            intent = ''
             voice_note = self.read_voice_cmd()
             for key in self.config:
                 utterances = Utils.match_pattern(voice_note, self.config[key]['utterances'])
                 if utterances:
+                    intent = key
                     response = Utils.choose_random(self.config[key]['response'])
                     break
 
-            if key == 'intent_greeting':
+            if intent == 'intent_greeting':
                 Greeting(self.logger, response).speak()
-                break
-            else:
-                Utils.playsound('Intent not found')
+                session = True
+                continue
+            elif intent == 'intent_applications':
+                if session:
+                    applications = self.config[key]['applications']
+                    Applications(logger=self.logger, response=response, applications=applications,
+                                 command=voice_note).launch()
+                    session = False
+                    continue
---
+++ intents/applications.py
@@ -0,0 +1,31 @@
+import os
+
+from utils.utils import Utils
+
+
+class Applications:
+    def __init__(self, logger, response, applications, command):
+        self.logger = logger
+        self.response = response
+        self.applications = applications
+        self.command = command
+
+    def get_name(self):
+        return self.command.split(' ')[1]
+
+    def get_path(self):
+        name = self.get_name()
+        self.logger.info('App Name : {}'.format(name))
+
+        for app, path in self.applications.items():
+            if name.lower() == app.lower():
+                return path
+
+    def launch(self):
+        path = self.get_path()
+        self.logger.info('App path : {}'.format(path))
+        if path:
+            Utils.playsound(self.response)
+            os.system('open {}'.format(path))
+        else:
+            Utils.playsound('I am sorry Sir. The app which you are looking for, is not register into my database.')
